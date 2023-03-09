c5e72474be8bcf5a0952951d67f0333f54a5a487
arosyidg <rosyidkeren164@gmail.com>
2023-03-09 09:34:38 +0800

    add some assets etc.

--- tscn/Code.gd
+++ tscn/Code.gd
@@ -1,4 +1,5 @@
-extends Sprite
+extends  Label
+#extends CharacterBody2D
 
 # Declare member variables here. Examples:
 # var a = 2
@@ -7,8 +8,11 @@ extends Sprite
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
+#func _physics_process(delta):
+#	self.set_global_position(get_global_mouse_position())
 
 func mulai():
+	
 	print(self.name)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
